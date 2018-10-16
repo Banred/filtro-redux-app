6e348cec1715c07615d334b81efd6e83936fdf0f
Edson <edanbasu@gmail.com>
2018-10-16 15:05:26 -0500

    primer commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import * as fromTodo from './todo.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducers';
+
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styles: []
+})
+export class TodoComponent implements OnInit {
+
+  completado = false;
+
+  constructor(
+    private store: Store<AppState>
+  ) { }
+
+  ngOnInit() {
+  }
+
+  toggleAll() {
+    this.completado = !this.completado;
+    const accion = new fromTodo.ToggleAllTodoAction(this.completado);
+    this.store.dispatch(accion);
+  }
+
+}
